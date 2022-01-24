b17412733843b633c2062e11af0d89d2593e620d
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-01-24 12:44:45 +0100

    add scale selector

---
+++ src/Scale.jsx
@@ -0,0 +1,8 @@
+export function Scale ({notes, selected_note_index, scale_intervals}) {
+  const ordered_notes = [...notes.slice(selected_note_index), ...notes.slice(0, selected_note_index)]
+  let scale_notes = []
+  scale_intervals.forEach(scale_interval => {
+    scale_notes.push(ordered_notes[scale_interval])
+  })
+  return scale_notes
+}
