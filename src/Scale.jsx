d806481bf8322117d457c0e034b83fbf7b576816
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-01-24 12:52:51 +0100

    front on notes displayed

--- src/Scale.jsx
+++ src/Scale.jsx
@@ -1,8 +1,12 @@
+import './stylesheets/scale.css'
+
 export function Scale ({notes, selected_note_index, scale_intervals}) {
   const ordered_notes = [...notes.slice(selected_note_index), ...notes.slice(0, selected_note_index)]
   let scale_notes = []
   scale_intervals.forEach(scale_interval => {
     scale_notes.push(ordered_notes[scale_interval])
   })
-  return scale_notes
+  return <div className="notes-displayer">
+    {scale_notes.join("-")}
+  </div>
 }
