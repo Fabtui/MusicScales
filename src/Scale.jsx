8a489b4e6b83f222ebb4e573891d6a1f2e71e8b9
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-01-24 16:41:51 +0100

    add some front

--- src/Scale.jsx
+++ src/Scale.jsx
@@ -8,3 +8,14 @@ export function Scale ({notes, selected_note_index, scale_intervals}) {
   })
   return scale_notes
 }
+
+export function ScaleDisplay ({notes, selected_note_index, scale_intervals}) {
+  const ordered_notes = [...notes.slice(selected_note_index), ...notes.slice(0, selected_note_index)]
+  let scale_notes = []
+  scale_intervals.forEach(scale_interval => {
+    scale_notes.push(ordered_notes[scale_interval])
+  })
+  return <div className="notes-displayer mb-4">
+    {scale_notes.join("-")}
+  </div>
+}
--- src/Selector.jsx
+++ src/Selector.jsx
@@ -3,6 +3,7 @@ import './stylesheets/selector.css'
 import {NoteDropDown} from './NoteDropdown'
 import {ScaleDropDown} from './ScaleDropDown'
 import {Scale} from './Scale'
+import {ScaleDisplay} from './Scale'
 import {ScaleTable} from './ScaleTable'
 import { NOTES } from './data'
 import { SCALES } from './data'
@@ -14,30 +15,37 @@ export class Selector extends Component {
     this.state = {
       selected_note_index: 0,
       selected_scale: SCALES_LIST["Major"],
-      selected_scale_name: SCALES[0]
+      selected_scale_name: SCALES[0],
+      selected_scale_notes: ["A", "B", "C#", "D", "E", "F#", "G#"]
     }
     this.handleNoteChange = this.handleNoteChange.bind(this)
     this.handleScaleChange = this.handleScaleChange.bind(this)
   }
 
   handleNoteChange (note) {
-    this.setState({selected_note_index: note})
+    this.setState({
+      selected_note_index: note,
+    })
+    console.log(this.state);
   }
 
   handleScaleChange (scale) {
-    this.setState({selected_scale_name: SCALES[scale]})
+    this.setState({
+      selected_scale_name: SCALES[scale],
+      selected_scale_notes: Scale({notes: NOTES, selected_note_index: this.state.selected_note_index, scale_intervals: this.state.selected_scale})
+    })
+    console.log(this.state);
   }
 
   render () {
-    const selected_scale_notes = Scale ({notes: NOTES, selected_note_index: this.state.selected_note_index, scale_intervals: this.state.selected_scale})
     const selected_note = NOTES[this.state.selected_note_index]
     return <div className='container'>
+      <ScaleDisplay notes={NOTES} selected_note_index={this.state.selected_note_index} scale_intervals={SCALES_LIST[this.state.selected_scale_name]}/>
+      <ScaleTable notes={NOTES} selected_scale_notes={this.state.selected_scale_notes} selected_note={selected_note} selected_note_index={this.state.selected_note_index}/>
       <div className="selectors">
         <NoteDropDown className="note-selector" selected_note={selected_note} notes={NOTES} onChange={this.handleNoteChange}/>
         <ScaleDropDown className="scale-selector" selected_scale={this.state.selected_scale_name} scales={SCALES} onChange={this.handleScaleChange}/>
       </div>
-      {/* <Scale notes={NOTES} selected_note_index={this.state.selected_note_index} scale_intervals={SCALES_LIST[this.state.selected_scale_name]}/> */}
-      <ScaleTable notes={NOTES} selected_scale_notes={selected_scale_notes} selected_note={selected_note} selected_note_index={this.state.selected_note_index}/>
     </div>
   }
 }
