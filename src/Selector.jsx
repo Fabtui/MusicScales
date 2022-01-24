9af76eeec30a65818216442d3828e3b5f93a3d8e
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-01-24 11:06:24 +0100

    add dropdown

---
+++ src/Selector.jsx
@@ -0,0 +1,41 @@
+import React, {Component} from 'react'
+import './stylesheets/clock.css'
+
+const NOTES = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+
+function DropdownItems ({note, index}) {
+  return <li><a className="dropdown-item" id={index} href="#">{note}</a></li>
+}
+
+function DropDown ({notes}) {
+  const items = []
+  notes.forEach((note, index) => {
+    items.push(<DropdownItems key={index} index={index} note={note}/>)
+  })
+  return <div className='note-selector'>
+      <div className="dropdown">
+        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+          Note
+        </button>
+        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+          {items}
+        </ul>
+      </div>
+    </div>
+}
+
+
+export class Selector extends Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      selected_note: 0
+    }
+  }
+
+  render () {
+    return <div className='note-selector'>
+      <DropDown notes={NOTES}/>
+    </div>
+  }
+}
