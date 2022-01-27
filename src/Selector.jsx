7b389673fbd8224022f93b69d29aef86891344a4
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-01-27 12:26:58 +0100

    front display

--- src/Selector.jsx
+++ src/Selector.jsx
@@ -54,10 +54,10 @@ export class Selector extends Component {
       <div className="scale-table">
         <ScaleTable selected_scale={this.state.selected_scale} notes={NOTES} selected_scale_notes={selected_scale_notes} selected_note={selected_note} selected_note_index={this.state.selected_note_index} selected_scale_notes={selected_scale_notes}/>
       </div>
+        <NeckTable selected_scale_notes={selected_scale_notes} selected_note_index={this.state.selected_note_index} selected_scale={this.state.selected_scale} />
       <div className="triads-container">
         <Triads selected_scale_notes={selected_scale_notes} selected_scale={this.state.selected_scale}/>
       </div>
-        <NeckTable selected_scale_notes={selected_scale_notes} selected_note_index={this.state.selected_note_index} selected_scale={this.state.selected_scale} />
     </div>
   }
 }
