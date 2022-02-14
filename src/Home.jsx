919eeba62bc5979089ae4b0978cff3123857ab39
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-02-14 20:51:50 +0100

    add hint boxe in home pages links

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,12 +5,15 @@ export function Home () {
   return <div className='home-wrap'>
       <Link className="home-wrap-link" to="/MyScale">
         <div className='home-wrap-item item1'>My scale</div>
+        <div className='hint-box'>All about a scale</div>
       </Link>
       <Link className="home-wrap-link" to="/AllScales">
         <div className='home-wrap-item item2'>All scales</div>
+        <div className='hint-box'>See all the existing scales</div>
       </Link>
       <Link className="home-wrap-link" to="/Search">
         <div className='home-wrap-item item3'>Search</div>
+        <div className='hint-box'>Find a scale by notes</div>
       </Link>
     </div>
 
