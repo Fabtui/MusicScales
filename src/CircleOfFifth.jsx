0613a396c656c601f2a4cc3d77928d8989076f89
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-03-03 10:49:04 +0100

    add circle in background on circle page

--- src/CircleOfFifth.jsx
+++ src/CircleOfFifth.jsx
@@ -3,6 +3,6 @@ import circle from './images/The-Circle-of-Fifths.png';
 export function CircleOfFifth () {
 
   return <div className='container circle-of-fifth'>
-           <img src={circle} alt="circle-of-fifth" />
+           {/* <img src={circle} alt="circle-of-fifth" /> */}
          </div>
 }
