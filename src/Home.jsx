7343a88b820b289658cf64f8ba7ef15463c437dd
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-03-15 19:36:00 +0100

    add orange and grey

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,19 +12,19 @@ export function Home () {
         <div className='hint-box'>See all the existing scales</div>
       </Link>
       <Link className="home-wrap-link" to="/SearchByNotes">
-        <div className='home-wrap-item item3'><h2>SEARCH BY NOTES</h2></div>
+        <div className='home-wrap-item item2'><h2>SEARCH BY NOTES</h2></div>
         <div className='hint-box'>Find a scale by notes</div>
       </Link>
       <Link className="home-wrap-link" to="/SearchByChords">
-        <div className='home-wrap-item item4'><h2>SEARCH BY CHORDS</h2></div>
+        <div className='home-wrap-item item1'><h2>SEARCH BY CHORDS</h2></div>
         <div className='hint-box'>Find a scale by chords</div>
       </Link>
       <Link className="home-wrap-link" to="/ChordsSearch">
-        <div className='home-wrap-item item5'><h2>SEARCH A CHORD</h2></div>
+        <div className='home-wrap-item item1'><h2>SEARCH A CHORD</h2></div>
         <div className='hint-box'>Find a chord</div>
       </Link>
       <Link className="home-wrap-link" to="/CircleOfFifth">
-        <div className='home-wrap-item item6'><h2>CIRCLE OF FIFTH</h2></div>
+        <div className='home-wrap-item item2'><h2>CIRCLE OF FIFTH</h2></div>
         <div className='hint-box'>Explore the circle of fifth</div>
       </Link>
     </div>
