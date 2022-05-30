64b9cd386fb66fa316a56b51bb8a04f4f8882671
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-05-30 19:22:25 +0200

    put selector into myscale function

---
+++ src/MyScale.jsx
@@ -0,0 +1,9 @@
+import { Selector } from "./Selector";
+import React from 'react';
+
+export function MyScale ({}) {
+
+  return <React.Fragment>
+          <Selector/>
+        </React.Fragment>
+}
